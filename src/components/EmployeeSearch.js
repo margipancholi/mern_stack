f3d67c8574cb365fcbd64c1a9430f4ce9cc07995
Dhruvesh <dhruvesh.baria@actonate.com>
2022-07-25 19:05:13 +0530

    feat:Table data change

--- src/components/EmployeeSearch.js
+++ src/components/EmployeeSearch.js
@@ -17,6 +17,8 @@ const GET_Employees = gql`
       lastName
       _id
       designation
+      employeeType
+      currentStatus
     }
   }
 `;
@@ -65,56 +67,51 @@ export default function EmployeeSearch() {
                       scope="col"
                       className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                     >
+                      No.
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
                       Name
                     </th>
                     <th
                       scope="col"
                       className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      Title
+                      Designation
                     </th>
                     <th
                       scope="col"
                       className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      Email
+                      EmployeeType
                     </th>
                     <th
                       scope="col"
                       className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      Role
-                    </th>
-                    <th
-                      scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                    >
-                      <span className="sr-only">Edit</span>
+                      Current Status
                     </th>
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {data.employees.map((person) => (
+                  {data.employees.map((person, index) => (
                     <tr key={person._id}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                        {index + 1}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                         {person.firstName}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {person.lastName}
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                        {person._id}
                       </td>
                       <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                         {person.designation}
                       </td>
-                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          Edit<span className="sr-only">, {person.name}</span>
-                        </a>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {person.employeeType}
+                      </td>
+                      <td className=" whitespace-nowrap py-4 pl-3 pr-4 text-sm text-gray-500">
+                        {person.currentStatus}
                       </td>
                     </tr>
                   ))}
